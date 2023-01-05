32d620695ea3a1013246499730fc8eda81749dd5
MphatsoMphepo <bsc-inf-20-19@unima.ac.mw>
2023-01-06 00:33:50 +0200

    Updated the frontend

---
+++ Maintenance-Backend/src/main/java/com/maintenance/project/FaultPackage/entity/Report.java
@@ -0,0 +1,19 @@
+package com.maulidi.login.FaultPackage.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "Faults")
+public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String faultName;
+    private String description;
+    private String roomNo;
+    private String dateOccured;
+}
